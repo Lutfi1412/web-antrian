d274da26b6c60b127684e62379e19973f4a89402
Lutfi1412 <122511487+Lutfi1412@users.noreply.github.com>
2024-12-10 00:20:48 +0700

    console insert

--- src/components/services/addQueue.tsx
+++ src/components/services/addQueue.tsx
@@ -5,6 +5,7 @@ export const addQueueNumber = async (queueNumber: number): Promise<void> => {
     await axios.post("https://api-antrian.vercel.app/api/insert", {
       queue_number: queueNumber,
     });
+    console.log("Queue number added successfully : ", queueNumber);
   } catch (error) {
     console.error("Error adding queue number:", error);
     throw error; // Rethrow to handle it in the component if needed
